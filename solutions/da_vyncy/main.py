ce43cf14793515263924483db0e2f08fbe1eae0b
Zadoev Roman <zadoev@gmail.com>
2015-11-29 00:09:51 +0600

    better version of da vyncy. In my bench it 20x faster, in codeeval it's faster previous only 6 times. And yes, here memory overhead.

--- solutions/da_vyncy/main.py
+++ solutions/da_vyncy/main.py
@@ -1,50 +1,155 @@
 __author__ = 'zadoev@gmail.com'
 """
-sequens aabaa;aaa;aacaa - depends of order
+sequence aabaa;aaa;aacaa - depends of order
 
 """
 
-
+from itertools import permutations
 import sys
 
-def commonOverlapIndexOf(text1, text2):
+
+def get_overlap(a, b):
     """
     From https://neil.fraser.name/news/2010/11/04/
+    with small optimizations about 3% speed up
 
+
+    >>> (get_overlap('a', 'b'), get_overlap('', ''))
+    (0, 0)
+    >>> get_overlap('a', 'ab')
+    1
+    >>> get_overlap('aa', 'ab')
+    1
+    >>> get_overlap('ab', 'aa')
+    0
+    >>> get_overlap('ababcab', 'bcab')
+    4
     """
-    # Cache the text lengths to prevent multiple calls.
-
-    text1_length = len(text1)
-    text2_length = len(text2)
+    a_length = len(a)
+    b_length = len(b)
     min_len = 0
-    # Eliminate the null case.
-    if text1_length == 0 or text2_length == 0:
+
+    if a_length == 0 or b_length == 0:
         return 0
-        # Truncate the longer string.
-    if text1_length > text2_length:
-        text1 = text1[-text2_length:]
-        min_len = text2_length
-    elif text1_length < text2_length:
-        text2 = text2[:text1_length]
-        min_len = text1_length
-        # Quick check for the worst case.
-    if text1 == text2:
+
+    if a_length > b_length:
+        a = a[-b_length:]
+        min_len = b_length
+    elif a_length < b_length:
+        b = b[:a_length]
+        min_len = a_length
+    if a == b:
         return min_len
 
-        # Start by looking for a single character match
-    # and increase length until no match is found.
     best = 0
     length = 1
-    f = text2.find
+    f = b.find
     while True:
-        pattern = text1[-length:]
+        pattern = a[-length:]
         found = f(pattern)
         if found == -1:
             return best
         length += found
-        if pattern == text2[:length]:
+        if pattern == b[:length]:
             best = length
             length += 1
+
+
+class Item(object):
+    def __init__(self, fragment):
+        self.fragment = fragment
+        self.connections = {}
+        self.best_item = None
+        self.best_overlap = 0
+
+    def connect(self, another_item, overlap=None):
+        """
+        Make connection from current item to another
+
+        >>> a = Item('ab')
+        >>> a.best_overlap
+        0
+        >>> a.best_item is None
+        True
+        >>> b = Item('a')
+        >>> a.connect(b)
+        >>> a.best_overlap
+        1
+        >>> a.best_item == b
+        True
+        >>> a.connections[b] == 1
+        True
+        >>> a = Item('')
+        >>> b = Item('')
+        >>> a.connect(b)
+        >>> not a.connections
+        True
+
+        :param another_item:
+        :type another_item: Item
+        :param overlap:
+        :type overlap: int
+        """
+
+        if another_item == self:
+            return
+
+        if overlap is None:
+            overlap = get_overlap(another_item.fragment, self.fragment)
+
+        if overlap == 0:
+            return
+
+        self.connections[another_item] = overlap
+
+        if overlap > self.best_overlap:
+            self.best_item = another_item
+            self.best_overlap = overlap
+
+    def disconnect(self, another):
+        """
+        Removes connection from this object to another
+
+        >>> a = Item('a')
+        >>> b = Item('b')  # fragments here not important
+        >>> c = Item('c')
+        >>> a.connect(b, 1)
+        >>> a.connect(c, 2)  # c best match
+        >>> (a.best_overlap, a.best_item == c)
+        (2, True)
+        >>> a.disconnect(c)
+        >>> (a.best_overlap, a.best_item==b)
+        (1, True)
+
+
+        :param another:
+        :type another: Item
+        :return:
+        """
+        is_best_deleted = self.best_item == another
+
+        del self.connections[another]
+
+        if is_best_deleted:
+            self.rebuild_best()
+
+    def rebuild_best(self):
+        """
+        called when best item deleted
+        >>> a = Item('')
+        >>> a.connections = {'a': 1, 'b': 2, 'c': 3}
+        >>> a.rebuild_best()
+        >>> (a.best_item, a.best_overlap)
+        ('c', 3)
+        """
+        if not self.connections:
+            self.best_item = None
+            self.best_overlap = 0
+            return
+
+        self.best_item, self.best_overlap = sorted(
+            self.connections.items(), key=lambda t: t[0], reverse=True
+        )[0]
 
 
 class Solver(object):
@@ -53,95 +158,75 @@ class Solver(object):
         self.len = len(self.fragments)
         self.prev_best = None
 
-    def find_best(self):
-        self.prev_best = None
-        pairs = (None, None)
-        max_overlap = 0
+    @staticmethod
+    def glue(a, b, overlap):
+        return a + b[overlap:]
 
-        for i in range(self.len -1):
-            for j in range(i+1, self.len):
-                a = self.fragments[i]
-                b = self.fragments[j]
-                reversed, overlap = self.overlap(a, b)
-                if overlap > max_overlap:
-                    if pairs != (None, None):
-                        self.prev_best = pairs
+    @staticmethod
+    def find_best(items):
+        """
+        Find longest overlap
 
-                    max_overlap = overlap
-                    if reversed:
-                        pairs = (j, i)
-                    else:
-                        pairs = (i, j)
+        :param items: fragments tree
+        :type items: list[Item]
+        :return:
+        """
 
-        return max_overlap, pairs[0], pairs[1]
+        if not items:
+            return ()
 
-    def find_best_partial(self):
-        if self.prev_best is None:
-            return 0, None, None
+        longest = sorted(items, key=lambda i: i.best_overlap, reverse=True)[0]
 
-        pairs = self.prev_best
-        self.prev_best = None
-        max_overlap = 0
+        if longest.best_overlap == 0:
+            return ()
 
-        for i in range(self.len -1):
-            j = self.len -1
-            a = self.fragments[i]
-            b = self.fragments[j]
-            reversed, overlap = self.overlap(a, b)
-            if overlap > max_overlap:
-
-                if pairs != (None, None):
-                    self.prev_best = pairs
-
-                if reversed:
-                    pairs = (j, i)
-                else:
-                    pairs = (i, j)
-
-                max_overlap = overlap
-
-        return max_overlap, pairs[0], pairs[1]
-
-    def glue(self, overlap, a, b):
-        return self.fragments[a] + self.fragments[b][overlap:]
-
-    def overlap(self, a, b):
-        overlap = commonOverlapIndexOf(a, b)
-        back_overlap = commonOverlapIndexOf(b, a)
-
-        if ( back_overlap > overlap ):
-            return True, back_overlap
-        else:
-            return False, overlap
+        return longest, longest.best_item, longest.best_overlap
 
     def solve(self):
-        while self.fragments:
-            overlap, a, b = self.find_best()
-            if overlap == 0:
-                return sorted(self.fragments, reverse=True, key=len)[0]
-            r = self.glue(overlap, a, b)
 
-            del self.fragments[max(a,b)]
-            del self.fragments[min(a, b)]
-            self.fragments.append(r)
+        # init data for solve
+        items_map = {f: Item(f) for f in self.fragments}
 
-            self.len -= 1
+        for (a, b) in permutations(self.fragments, 2):
+            overlap = get_overlap(a, b)
 
-            overlap, a, b = self.find_best_partial()
+            if overlap:
+                items_map[b].connect(items_map[a], overlap)
 
-            if overlap == 0:
-                continue
+        items = items_map.values()
 
-            r = self.glue(overlap, a, b)
-            del self.fragments[max(a, b)]
-            del self.fragments[min(a, b)]
-            self.fragments.append(r)
-            self.len -= 1
+        del items_map
 
-        return ''.join(self.fragments)
+        while True:
+            best = self.find_best(items)
+
+            if not best:
+                break
+
+            a, b, overlap = best
+
+            result = Item(self.glue(b.fragment, a.fragment, overlap))
+
+            for i in items:
+                if a in i.connections:
+                    i.disconnect(a)
+                    i.connect(result)
+
+                if b in i.connections:
+                    i.disconnect(b)
+                    i.connect(result)
+
+            for i in b.connections.keys():
+                result.connect(i)
+
+            items.remove(a)
+            items.remove(b)
+            items.append(result)
+
+        return sorted([i.fragment for i in items], reverse=True, key=len)[0]
+
 
 if __name__ == '__main__':
-    # print commonOverlapIndexOf('abbbc', 'bbbd')
     with open(sys.argv[1], 'r') as test_cases:
         for line in test_cases:
             solver = Solver(line.strip().split(';'))

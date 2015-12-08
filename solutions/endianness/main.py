23c608a389d46b667315c8013a949874e4466645
Roman Zadoev <zadoev@gmail.com>
2015-12-08 10:46:41 +0600

    endianness 60.221 score

---
+++ solutions/endianness/main.py
@@ -0,0 +1,21 @@
+__author__ = 'zadoev@gmail.com'
+"""
+Write a program which prints the endianness of the system.
+
+INPUT SAMPLE:
+
+There is no input for this program.
+
+OUTPUT SAMPLE:
+
+Print to stdout the endianness, wheather it is LittleEndian or BigEndian.
+
+For example:
+
+BigEndian
+"""
+
+import sys
+
+if __name__ == '__main__':
+    print '{}Endian'.format(sys.byteorder.capitalize())

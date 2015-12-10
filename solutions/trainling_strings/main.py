ff827eeaaf2b10e60ee52975b3199d120b5f2aef
Roman Zadoev <zadoev@gmail.com>
2015-12-10 12:14:05 +0600

    trailing strings, medium with 59.028 (slice faster then string.endswith), version with endswith get 54 score

---
+++ solutions/trainling_strings/main.py
@@ -0,0 +1,36 @@
+__author__ = 'zadoev@gmail.com'
+"""
+There are two strings: A and B. Print 1 if string B occurs at the end of
+string A. Otherwise, print 0.
+
+INPUT SAMPLE:
+
+The first argument is a path to the input filename containing two
+comma-delimited strings, one per line. Ignore all empty lines in the input file.
+
+For example:
+
+Hello World,World
+Hello CodeEval,CodeEval
+San Francisco,San Jose
+OUTPUT SAMPLE:
+
+Print 1 if the second string occurs at the end of the first string. Otherwise, print 0.
+
+For example:
+
+1
+1
+0
+"""
+import sys
+
+bool_to_int = (0, 1)
+
+if __name__ == '__main__':
+    with open(sys.argv[1]) as test_cases:
+        for line in test_cases:
+            line = line.rstrip()
+            if line:
+                a, b = line.split(',')
+                print bool_to_int[a[-len(b):] == b]

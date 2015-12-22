acdbef8241071fe9b01d480345aa17147a6ae7dc
Roman Zadoev <zadoev@gmail.com>
2015-12-22 10:47:16 +0600

    hard, Minesweeper time: 143 memory: 4490271 points: 88.577

--- solutions/minesweeper/main.py
+++ solutions/minesweeper/main.py
@@ -1,5 +1,31 @@
 __author__ = 'zadoev@gmail.com'
+"""
+MINESWEEPER
+CHALLENGE DESCRIPTION:
 
+You will be given an M*N matrix. Each item in this matrix is either a '*'
+or a '.'. A '*' indicates a mine whereas a '.' does not. The objective of
+the challenge is to output a M*N matrix where each element contains a number
+(except the positions which actually contain a mine which will remain as '*')
+which indicates the number of mines adjacent to it. Notice that each position
+has at most 8 adjacent positions e.g. left, top left, top, top right, right, ...
+
+INPUT SAMPLE:
+
+Your program should accept as its first argument a path to a filename. Each
+line in this file contains M,N, a semicolon and the M*N matrix in row major
+form. E.g.
+
+3,5;**.........*...
+4,4;*........*......
+OUTPUT SAMPLE:
+
+Print out the new M*N matrix (in row major form) with each position(except
+the ones with the mines) indicating how many adjacent mines are there. E.g.
+
+**100332001*100
+*10022101*101110
+"""
 import sys
 
 

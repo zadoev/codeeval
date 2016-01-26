bcf2f90f32f1428d24a09e43beefd4dd66649f75
Roman Zadoev <zadoev@gmail.com>
2016-01-26 18:02:39 +0600

    py3, medium, decimal to binary 55 points

---
+++ solutions/decimal_to_binary/main.py3.py
@@ -0,0 +1,36 @@
+"""
+You are given a decimal (base 10) number, print its binary representation.
+
+INPUT SAMPLE:
+
+Your program should accept as its first argument a path to a filename containing
+decimal numbers greater or equal to 0, one per line.
+
+Ignore all empty lines.
+
+For example:
+
+2
+10
+67
+OUTPUT SAMPLE:
+
+Print the binary representation, one per line.
+
+For example:
+
+10
+1010
+1000011
+
+"""
+
+import sys
+
+
+if __name__ == '__main__':
+    with open(sys.argv[1]) as test_cases:
+        for line in test_cases:
+            data = line.rstrip()
+            if data:
+                print(bin(int(data))[2:])
